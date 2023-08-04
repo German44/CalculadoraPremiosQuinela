11d9bc7c6e31bf4cf05ae398504b9435ff495c70
German Ravarotto <g44ravarotto@gmail.com>
2023-08-04 13:59:56 -0300

    Update app.js
    agrado pregunta final

--- app.js
+++ app.js
@@ -1,53 +1,71 @@
+let recalculo = false;
 
-let diaDeSemana = prompt('Es Dia de Semana?');
+while (recalculo != true) {
 
-if (diaDeSemana === 'si') {
-    alert('Te Calculare el premio a la Cabeza como dia de semana');
+    let diaDeSemana = prompt('Es Dia de Semana?');
 
-    let apuesta = prompt('Cuanto dinero aposto?');
-    let numeroCifra = prompt('Cuantos cifras de apuesta?');
+    if (diaDeSemana === 'si') {
+        alert('Te Calculare el premio a la Cabeza como dia de semana');
 
-    function cifra(numeroCifra) {
-        switch (numeroCifra) {
-            case '1':
-                alert('Tu premio es ' + (apuesta * 7));
-                break;
-            case '2':
-                alert('Tu premio es ' + (apuesta * 70));
-                break;
-            case '3':
-                alert('Tu premio es ' + (apuesta * 600));
-                break;
-            default:
-                alert('Tu premio es 0');
+        let apuesta = prompt('Cuanto dinero aposto?');
+        let numeroCifra = prompt('Cuantos cifras de apuesta?');
+
+        cifra = (numeroCifra) => {
+            switch (numeroCifra) {
+                case '1':
+                    alert('Tu premio es ' + (apuesta * 7));
+                    break;
+                case '2':
+                    alert('Tu premio es ' + (apuesta * 70));
+                    break;
+                case '3':
+                    alert('Tu premio es ' + (apuesta * 600));
+                    break;
+                default:
+                    alert('Tu premio es 0');
+            }
         }
+
+        cifra(numeroCifra);
+
+    } else if (diaDeSemana === 'no') {
+        alert('Te Calculare el premio a la Cabeza como dia Sabado');
+
+        let apuesta = prompt('Cuanto dinero aposto?');
+        let numeroCifra = prompt('Cuantos cifras de apuesta?');
+
+        function cifra(numeroCifra) {
+            switch (numeroCifra) {
+                case '1':
+                    return alert('Tu premio es ' + (apuesta * 7));
+
+                case '2':
+                    return alert('Tu premio es ' + (apuesta * 70));
+
+                case '3':
+                    return alert('Tu premio es ' + (apuesta * 700));
+
+                default:
+                    alert('Tu premio es 0');
+            }
+        }
+
+        cifra(numeroCifra);
+
+    } else {
+        alert('por favor ingrese si o no');
     }
 
-    cifra(numeroCifra);
-} else if (diaDeSemana === 'no') {
-    alert('Te Calculare el premio a la Cabeza como dia Sabado');
+    pregunta = prompt('Desea hacer otro calculo?')
 
-    let apuesta = prompt('Cuanto dinero aposto?');
-    let numeroCifra = prompt('Cuantos cifras de apuesta?');
-
-    function cifra(numeroCifra) {
-        switch (numeroCifra) {
-            case '1':
-                return alert('Tu premio es ' + (apuesta * 7));
-
-            case '2':
-                return alert('Tu premio es ' + (apuesta * 70));
-
-            case '3':
-                return alert('Tu premio es ' + (apuesta * 700));
-
-            default:
-                alert('Tu premio es 0');
-        }
-    }
-
-    cifra(numeroCifra);
-
-} else {
-    alert('por favor ingrese si o no');
+    if (pregunta === 'si') {
+        recalculo = false;
+    } else if (pregunta === 'no') {
+        alert('chau')
+        recalculo = true;
+    };
 }
+
+
+
+
