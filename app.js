72f460d3a3f4f975ce16396ba4296d7c66b49a4a
German Ravarotto <g44ravarotto@gmail.com>
2023-08-05 12:40:56 -0300

    Update app.js
    mejoras en los alerts y promps

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ let recalculo = false;
 cifraEnSemana = (numeroCifra, apuesta) => {
     switch (numeroCifra) {
         case '1':
-            alert('Tu premio es ' + (apuesta * 7));
+            alert('Tu premio a primera es $' + (apuesta * 7));
             break;
         case '2':
-            alert('Tu premio es ' + (apuesta * 70));
+            alert('Tu premio a primera es $' + (apuesta * 70));
             break;
         case '3':
-            alert('Tu premio es ' + (apuesta * 600));
+            alert('Tu premio a primera es $' + (apuesta * 600));
             break;
         default:
             alert('Coloca una cifra valida');
@@ -19,13 +19,13 @@ cifraEnSemana = (numeroCifra, apuesta) => {
 function cifraSabado(numeroCifra, apuesta) {
     switch (numeroCifra) {
         case '1':
-            return alert('Tu premio es ' + (apuesta * 7));
+            return alert('Tu premio a primera es $' + (apuesta * 7));
 
         case '2':
-            return alert('Tu premio es ' + (apuesta * 70));
+            return alert('Tu premio a primera es $' + (apuesta * 70));
 
         case '3':
-            return alert('Tu premio es ' + (apuesta * 700));
+            return alert('Tu premio a primera es $' + (apuesta * 700));
 
         default:
             alert('Coloca una cifra valida');
@@ -34,13 +34,13 @@ function cifraSabado(numeroCifra, apuesta) {
 
 while (recalculo != true) {
 
-    let diaDeSemana = prompt('Es Dia de Semana?');
+    let diaDeSemana = prompt('Es Dia de Semana?[si o no]');
 
     if (diaDeSemana === 'si') {
         alert('Te Calculare el premio a la Cabeza como dia de semana');
 
-        let apuesta = prompt('Cuanto dinero aposto?');
-        let numeroCifra = prompt('Cuantos cifras de apuesta?');
+        let apuesta = prompt('Cuanto dinero aposto?[1 a infinito]');
+        let numeroCifra = prompt('Cuantos cifras de apuesta?[1, 2 , 3]');
 
 
         cifraEnSemana(numeroCifra, apuesta);
@@ -58,12 +58,12 @@ while (recalculo != true) {
         alert('por favor ingrese si o no');
     }
 
-    pregunta = prompt('Desea hacer otro calculo?')
+    pregunta = prompt('Desea hacer otro calculo?[si o no]')
 
     if (pregunta === 'si') {
         recalculo = false;
     } else if (pregunta === 'no') {
-        alert('chau')
+        alert('Chau, Buena suerte')
         recalculo = true;
     };
 }
